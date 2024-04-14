8e1459b1532d07b89f93d5d1d6875468f533acfd
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2024-04-13 23:09:25 -0400

    create basic nodes and overall markdown object with an ast

---
+++ src/main/java/com/singh/backend/mdtohtml/model/MarkdownTreeNode.java
@@ -0,0 +1,11 @@
+package com.singh.backend.mdtohtml.model;
+
+import java.util.*;
+public class MarkdownTreeNode {
+
+    // getter setter
+    public List<MarkdownTree> markdownTrees;
+
+    public MarkdownTree() {
+    }
+}
